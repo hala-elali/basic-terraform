a11c7ea7fda66f5b2848dc86afbcac8fa9e9fa0b
hala-elali <142069462+hala-elali@users.noreply.github.com>
2026-02-10 02:39:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
-resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr
+terraform {
+  backend "s3" {
+    bucket         = "myorg-terraform-state-freetier2026-us-east-1"
+    key            = "envs/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+  resource "aws_vpc" "this" {
+    cidr_block = var.vpc_cidr
+  }
+
 }
+
 
